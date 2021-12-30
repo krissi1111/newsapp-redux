04250c36a4b6d28c612eeee4930ee411ff90f8c7
Ket1 <ket1@hi.is>
2021-12-30 14:07:19 +0000

    Added pagination

--- src/components/newsCards/NewsCards.js
+++ src/components/newsCards/NewsCards.js
@@ -1,9 +1,9 @@
 import { useEffect } from "react";
-import { Container, Spinner } from "react-bootstrap";
+import { ButtonGroup, ButtonToolbar, Card, Container, Dropdown, DropdownButton, Pagination, Spinner } from "react-bootstrap";
 import { useSelector, useDispatch } from 'react-redux';
 import { NewsCardList } from "./NewsCardList";
 import { NewsModal } from './NewsModal';
-import { getNewsSearch } from "../../redux/slices/newsDataSlice";
+import { changeItemPerPage, changePage, getNewsSearch } from "../../redux/slices/newsDataSlice";
 
 export function NewsCardContainer() {
   const newsData = useSelector((state) => state.newsCard.newsData)
@@ -15,21 +15,52 @@ export function NewsCardContainer() {
   }, [dispatch, apiStatus])
 
   return (
-    <>
-      <Container style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '50rem'}}>
-        {apiStatus !== 'loaded' ?(
-          <Spinner animation="border"/>
-        ) : newsData.length === 0 ?(
-          <h1>No data</h1>
-        ) : (
-          <>
-            <NewsCardList newsData={newsData}/>
-            
-          </>
-        )}
-        <NewsModal/>
+      <Container fluid style={{ justifyContent: 'center', minHeight: '50rem', flexDirection: 'column', width:'100%'}}>
+        <NewsCardPages/>
+        <NewsCardList newsData={newsData}/>
       </Container>
-    </>
+  )
+}
+
+export function NewsCardPages() {
+  const newsDataState = useSelector((state) => state.newsCard)
+  let { itemPerPage, pageCount, pageCurrent } = newsDataState
+  const dispatch = useDispatch();
+  
+  const pageCounts = [5, 10, 20, 50, 100]
+  let pageList = Array.from({length:pageCount},(v,k)=>k+1) 
+
+  return(
+    <ButtonToolbar style={{marginInline:'1%', justifyContent:'space-between'}}>
+      <Pagination>
+        {
+          pageList.map(page => 
+            <Pagination.Item 
+              key={page} 
+              active={page === pageCurrent + 1}
+              onClick={() => dispatch(changePage(page-1))}
+            >
+              {page}
+            </Pagination.Item>
+          )
+        }
+      </Pagination>
+      <Dropdown>
+        <DropdownButton as={ButtonGroup} title='Items per page'>
+          {
+            pageCounts.map(amount => 
+              <Dropdown.Item 
+                key={amount}
+                active={amount === itemPerPage}
+                onClick={() => dispatch(changeItemPerPage(amount))}
+              >
+                {amount}
+              </Dropdown.Item>
+              )
+          }
+        </DropdownButton>
+      </Dropdown>
+    </ButtonToolbar>
   )
 }
 
