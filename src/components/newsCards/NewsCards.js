2cd8d0251ac26182f3ca593d0f811697688e4458
Ket1 <ket1@hi.is>
2022-01-21 17:18:56 +0000

    Added pagination

--- src/components/newsCards/NewsCards.js
+++ src/components/newsCards/NewsCards.js
@@ -2,20 +2,26 @@ import { useEffect } from "react";
 import { ButtonGroup, ButtonToolbar, Card, Container, Dropdown, DropdownButton, Pagination, Spinner } from "react-bootstrap";
 import { useSelector, useDispatch } from 'react-redux';
 import { NewsCardList } from "./NewsCardList";
-import { NewsModal } from './NewsModal';
-import { changeItemPerPage, changePage, getNewsSearch } from "../../redux/slices/newsDataSlice";
+import { changeItemPerPage, changePage, getNewsAll } from "../../redux/slices/newsDataSlice";
+import { selectFavChecked, getUserFavs, selectLoggedIn } from "../../redux/slices/authSlice";
 
 export function NewsCardContainer() {
   const newsData = useSelector((state) => state.newsCard.newsData)
-  const apiStatus = useSelector((state) => state.newsCard.status)
+  const apiStatus = useSelector((state) => state.newsCard.newsStatus)
+  const favChecked = useSelector(selectFavChecked)
+  const loggedIn = useSelector(selectLoggedIn)
   const dispatch = useDispatch();
   
-  useEffect(() =>{
-    if(apiStatus === 'idle') dispatch(getNewsSearch())
+  useEffect(() => {
+    if(apiStatus === 'idle') dispatch(getNewsAll())
   }, [dispatch, apiStatus])
 
+  useEffect(() => {
+    if(!favChecked && loggedIn) dispatch(getUserFavs())
+  }, [dispatch, favChecked, loggedIn])
+
   return (
-      <Container fluid style={{ justifyContent: 'center', minHeight: '50rem', flexDirection: 'column', width:'100%'}}>
+      <Container fluid style={{ justifyContent: 'center', minHeight: '0rem', flexDirection: 'column', width:'100%'}}>
         <NewsCardPages style={{marginInline:'1%', justifyContent:'space-between'}}/>
         <NewsCardList newsData={newsData}/>
         <NewsCardPages className='mt-3' style={{marginInline:'1%', justifyContent:'space-between'}}/>
@@ -27,6 +33,7 @@ export function NewsCardPages(props) {
   const newsDataState = useSelector((state) => state.newsCard)
   let { itemPerPage, pageCount, pageCurrent } = newsDataState
   const dispatch = useDispatch();
+  const handlePageChange = (page) => {dispatch(changePage(page))}
   
   const pageCounts = [5, 10, 20, 50, 100]
   let pageList = Array.from({length:pageCount},(v,k)=>k+1) 
@@ -34,17 +41,43 @@ export function NewsCardPages(props) {
   return(
     <ButtonToolbar {...props}>
       <Pagination>
+        <Pagination.First onClick={() => handlePageChange(0)}/>
+        <Pagination.Prev onClick={() => handlePageChange(Math.max(pageCurrent-1, 0))}/>
         {
           pageList.map(page => 
-            <Pagination.Item 
-              key={page} 
-              active={page === pageCurrent + 1}
-              onClick={() => dispatch(changePage(page-1))}
-            >
-              {page}
-            </Pagination.Item>
+            ((page-3 <= pageCurrent) && (pageCurrent <= page+1)) ?(
+              <Pagination.Item 
+                key={page} 
+                active={page === pageCurrent + 1}
+                onClick={() => handlePageChange(page-1)}
+              >
+                {page}
+              </Pagination.Item>
+              ) : (page === 1) ?(
+                <Pagination.Item 
+                key={page} 
+                active={page === pageCurrent + 1}
+                onClick={() => handlePageChange(page-1)}
+              >
+                {page}
+              </Pagination.Item>
+              ) : (page === 2 && pageCurrent >= 4) || (page === pageCount-1 && pageCurrent <= pageCount-2) ?(
+              <Pagination.Ellipsis/>
+              ) : (page === pageCount) ?(
+                <Pagination.Item 
+                key={page} 
+                active={page === pageCurrent + 1}
+                onClick={() => handlePageChange(page-1)}
+              >
+                {page}
+              </Pagination.Item>
+              ) : (
+                <></>
+              )
           )
         }
+        <Pagination.Next onClick={() => handlePageChange(Math.min(pageCurrent+1, pageCount-1))}/>
+        <Pagination.Last onClick={() => handlePageChange(pageCount-1)}/>
       </Pagination>
       <Dropdown>
         <DropdownButton as={ButtonGroup} title='Items per page'>
