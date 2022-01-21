3e2b5a17e8bf4f7c8528d52b3a707b3fbafedce5
Ket1 <ket1@hi.is>
2022-01-21 17:23:53 +0000

    Changed list key to insure uniqueness

--- src/components/newsCards/NewsCardList.js
+++ src/components/newsCards/NewsCardList.js
@@ -7,12 +7,12 @@ import NewsModal from "./NewsModal";
 export function NewsCardList(props) {
   //let newsData = props.newsData;
   const newsDataState = useSelector((state) => state.newsCard)
-  const { status, itemPerPage } = newsDataState
+  const { newsStatus, itemPerPage } = newsDataState
   let newsData = newsDataState.newsDataView
 
   return (
     <>
-      {status !== 'loaded' ?(
+      {newsStatus !== 'loaded' ?(
         <Spinner animation="border"/>
       ) : newsData.length === 0 ?(
         <h1>No data</h1>
@@ -20,7 +20,7 @@ export function NewsCardList(props) {
         <>
           <CardGroup style={{display:'flex', justifyContent:'space-evenly'}}>
             {newsData.map(item => (
-              <NewsCardItem key={item.title} newsItem={item}/>
+              <NewsCardItem key={item.link} newsItem={item}/>
             ))}
           </CardGroup>
           <NewsModal/>
