2fe41d80f3f7de8ac15f1323b7aa81d62b5001f6
Ket1 <ket1@hi.is>
2022-03-17 13:24:19 +0000

    Added error reducer to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,18 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import newsCardReducer from './slices/newsDataSlice';
 import searchReducer from './slices/newsSearchSlice';
 import newsModalReducer from './slices/newsModalSlice';
 import commentsReducer from './slices/commentSlice'
 import authReducer from './slices/authSlice';
+import errorReducer from './slices/errorSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     newsCard: newsCardReducer,
     newsModal: newsModalReducer,
     search: searchReducer,
     comments: commentsReducer,
-    auth: authReducer
+    auth: authReducer,
+    error: errorReducer
   },
 });
