e18f89c803427af757441a64a32466c282b407cb
Ket1 <ket1@hi.is>
2022-03-17 13:29:05 +0000

    Added error handling and comment restoration

--- src/redux/slices/commentSlice.js
+++ src/redux/slices/commentSlice.js
@@ -1,5 +1,6 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { Comments, Reply } from '../../services/fetchAPI';
+import { showErrorModal } from './errorSlice';
 import { getNewsPopular } from './newsDataSlice';
 
 const initialState = {
@@ -9,89 +10,185 @@ const initialState = {
 
 export const getComments = createAsyncThunk(
   'comments/getComments',
-  async(newsId) => {
+  async(newsId, { rejectWithValue, fulfillWithValue, dispatch }) => {
     const form = new FormData()
     form.append('newsId', newsId)
     const response = await Comments.getList(form)
-    return response.json()
+    if(response.status !== 200) {
+      return response.json().then(res => {
+        dispatch(showErrorModal(res.message))
+        return rejectWithValue(res)
+      })
+    }
+    else return response.json().then(res => fulfillWithValue(res))
   }
 )
 
 export const addComment = createAsyncThunk(
   'comments/addComment',
-  async(arg, { dispatch }) => {
+  async(arg, { rejectWithValue, fulfillWithValue, dispatch }) => {
     const form = new FormData()
     form.append('newsId', arg.newsId)
     form.append('commentText', arg.text)
     const response = await Comments.add(form)
-    dispatch(getComments(arg.newsId))
-    dispatch(getNewsPopular())
-    return response.json()
+    if(response.status !== 200) {
+      return response.json().then(res => {
+        dispatch(showErrorModal(res.message))
+        return rejectWithValue(res)
+      })
+    }
+    else return response.json().then(res => {
+      dispatch(getComments(arg.newsId))
+      dispatch(getNewsPopular())
+      return fulfillWithValue(res)
+    })
   }
 )
 
 export const addReply = createAsyncThunk(
   'comments/addReply',
-  async(arg, { dispatch }) => {
+  async(arg, { rejectWithValue, fulfillWithValue, dispatch }) => {
     const form = new FormData()
     form.append('newsId', arg.newsId)
     form.append('replyText', arg.text)
     form.append('commentId', arg.commentId)
     const response = await Reply.add(form)
-    dispatch(getComments(arg.newsId))
-    return response.json()
+    if(response.status !== 200) {
+      return response.json().then(res => {
+        dispatch(showErrorModal(res.message))
+        return rejectWithValue(res)
+      })
+    }
+    else return response.json().then(res => {
+      dispatch(getComments(arg.newsId))
+      return fulfillWithValue(res)
+    })
   }
 )
 
 export const deleteComment = createAsyncThunk(
   'comments/deleteComment',
-  async(arg, { dispatch, getState }) => {
+  async(arg, { dispatch, getState, rejectWithValue, fulfillWithValue }) => {
     const form = new FormData()
     form.append('commentId', arg.commentId)
     const response = await Comments.delete(form)
-    let newsId =  getState().newsModal.item.id
-    dispatch(getComments(newsId))
-    dispatch(getNewsPopular())
-    return response.json()
+    if(response.status !== 200) {
+      return response.json().then(res => {
+        dispatch(showErrorModal(res.message))
+        return rejectWithValue(res)
+      })
+    }
+    else return response.json().then(res => {
+      let newsId = getState().newsModal.item.id
+      dispatch(getComments(newsId))
+      dispatch(getNewsPopular())
+      return fulfillWithValue(res)
+    })
   }
 )
 
 export const deleteReply = createAsyncThunk(
   'comments/deleteReply',
-  async(arg, { dispatch, getState }) => {
+  async(arg, { dispatch, getState, rejectWithValue, fulfillWithValue }) => {
     const form = new FormData()
     form.append('replyId', arg.replyId)
     const response = await Comments.deleteReply(form)
-    let newsId = getState().newsModal.item.id
-    dispatch(getComments(newsId))
-    dispatch(getNewsPopular())
-    return response.json()
+    if(response.status !== 200) {
+      return response.json().then(res => {
+        dispatch(showErrorModal(res.message))
+        return rejectWithValue(res)
+      })
+    }
+    else return response.json().then(res => {
+      let newsId = getState().newsModal.item.id
+      dispatch(getComments(newsId))
+      dispatch(getNewsPopular())
+      return fulfillWithValue(res)
+    })
+  }
+)
+
+export const restoreComment = createAsyncThunk(
+  'comments/restoreComment',
+  async(arg, { dispatch, getState, rejectWithValue, fulfillWithValue }) => {
+    const form = new FormData()
+    form.append('commentId', arg.commentId)
+    const response = await Comments.restore(form)
+    if(response.status !== 200) {
+      return response.json().then(res => {
+        dispatch(showErrorModal(res.message))
+        return rejectWithValue(res)
+      })
+    }
+    else return response.json().then(res => {
+      let newsId = getState().newsModal.item.id
+      dispatch(getComments(newsId))
+      dispatch(getNewsPopular())
+      return fulfillWithValue(res)
+    })
+  }
+)
+
+export const restoreReply = createAsyncThunk(
+  'comments/restoreReply',
+  async(arg, { dispatch, getState, rejectWithValue, fulfillWithValue }) => {
+    const form = new FormData()
+    form.append('replyId', arg.replyId)
+    const response = await Comments.restoreReply(form)
+    if(response.status !== 200) {
+      return response.json().then(res => {
+        dispatch(showErrorModal(res.message))
+        return rejectWithValue(res)
+      })
+    }
+    else return response.json().then(res => {
+      let newsId = getState().newsModal.item.id
+      dispatch(getComments(newsId))
+      dispatch(getNewsPopular())
+      return fulfillWithValue(res)
+    })
   }
 )
 
 export const editComment = createAsyncThunk(
   'comments/editComment',
-  async(arg, { dispatch, getState }) => {
+  async(arg, { dispatch, getState, rejectWithValue, fulfillWithValue }) => {
     const form = new FormData()
     form.append('commentId', arg.commentId)
     form.append('commentText', arg.text)
     const response = await Comments.edit(form)
-    let newsId = getState().newsModal.item.id
-    dispatch(getComments(newsId))
-    return response.json()
+    if(response.status !== 200) {
+      return response.json().then(res => {
+        dispatch(showErrorModal(res.message))
+        return rejectWithValue(res)
+      })
+    }
+    else return response.json().then(res => {
+      let newsId = getState().newsModal.item.id
+      dispatch(getComments(newsId))
+      return fulfillWithValue(res)
+    })
   }
 )
 
 export const editReply = createAsyncThunk(
   'comment/editReply',
-  async(arg, { dispatch, getState }) => {
+  async(arg, { dispatch, getState, rejectWithValue, fulfillWithValue }) => {
     const form = new FormData()
     form.append('replyId', arg.replyId)
     form.append('replyText', arg.text)
     const response = await Comments.editReply(form)
-    let newsId = getState().newsModal.item.id
-    dispatch(getComments(newsId))
-    return response.json()
+    if(response.status !== 200) {
+      return response.json().then(res => {
+        dispatch(showErrorModal(res.message))
+        return rejectWithValue(res)
+      })
+    }
+    else return response.json().then(res => {
+      let newsId = getState().newsModal.item.id
+      dispatch(getComments(newsId))
+      return fulfillWithValue(res)
+    })
   }
 )
 
@@ -110,7 +207,7 @@ export const commentSlice = createSlice({
     })
     .addCase(getComments.fulfilled, (state, action) => {
       state.status = 'loaded'
-      state.commentData = action.payload
+      state.commentData = action.payload.data
     })
   }
 })
